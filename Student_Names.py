cd6c1a836baa932cbcd60a6025efc79ba2373d9f
Gabriela Perez <gabrielaperezgil@utexas.edu>
2023-01-24 17:42:52 -0600

    found 20 students in the file

--- Student_Names.py
+++ Student_Names.py
@@ -1,8 +1,16 @@
 s = open('studentnames.txt', 'r')
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
-print(s.readline().rstrip('\n'))
+numStudents = 0
+for student in s:
+    name = student.rstrip('\n')
+    if len(name) > 0:
+        print(name)
+        numStudents += 1
+print("There were " + str(numStudents) + " students in the file.")
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+# print(s.readline().rstrip('\n'))
+
 s.close()
